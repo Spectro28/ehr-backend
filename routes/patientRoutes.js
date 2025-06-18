132d6fa48d66039d2b6a11e530d3b6126410a3b6
ELVIS1230 <vladimirortiz1230@gmail.com>
2025-06-18 00:44:22 -0500

    fix: arreglo ruta

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -9,6 +9,6 @@ router.delete('/:id', authenticateToken, patientController.deletePatient);
 router.get('/', authenticateToken, patientController.getPatients);
 router.get('/:id', authenticateToken, patientController.getPatientById);
 
-router.get('/patient/:typeIdentification/:identification', patientController.getPatientByIdentification);
+router.get('/:typeIdentification/:identification', patientController.getPatientByIdentification);
 
 module.exports = router;
